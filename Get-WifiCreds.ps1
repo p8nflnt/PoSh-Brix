5c9758f22544dd0e52efb6519692e799adb66485
Payton Flint <47405653+p8nflnt@users.noreply.github.com>
2023-10-27 12:32:57 -0500

    Create Get-WifiCreds.ps1

---
+++ Get-WifiCreds.ps1
@@ -0,0 +1,10 @@
+# display WiFi profiles
+netsh wlan show profile 
+
+# display WiFi profile info & plaintext creds
+netsh wlan show profile name= "<INSERT SSID>" key=clear
+
+# display all SSID/key pairs
+(netsh wlan show profiles) | Select-String "All User Profile\s+:\s+(.+)" | ForEach-Object {$ssid = $_.Matches.Groups[1].Value
+$key = (netsh wlan show profile name="$ssid" key=clear | Select-String "Key Content\s+:\s+(.+)").Matches.Groups[1].Value
+"SSID: $ssid`nKey:  $key`n-------------------------"}
