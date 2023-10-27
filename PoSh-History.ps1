babae2be94d8a9c735488066c071a3e5373dd7ed
Payton Flint <47405653+p8nflnt@users.noreply.github.com>
2023-10-27 13:11:44 -0500

    Create PoSh-History.ps1

---
+++ PoSh-History.ps1
@@ -0,0 +1,4 @@
+# PowerShell history:
+$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt
+# Example:
+\\<COMPUTERNAME>\C$\Users\<USERNAME>\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt
