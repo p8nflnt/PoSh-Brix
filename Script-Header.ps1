2fb211f5c6f0b6f7751667016c88bfd9a294009c
Payton Flint <47405653+p8nflnt@users.noreply.github.com>
2023-10-27 13:15:35 -0500

    Create Script-Header.ps1

---
+++ Script-Header.ps1
@@ -0,0 +1,13 @@
+<#
+.SYNOPSIS
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+
+.NOTES
+    Name: Lorem-Ipsum
+    Author: Rick Astley
+    Version: 1.0
+    DateCreated: 2020-Jan-1
+
+.LINK
+    https://youtu.be/dQw4w9WgXcQ?si=mFplzW-sd_qkIEB1
+#>
